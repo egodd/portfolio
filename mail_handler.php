e29802cb374876c87cba4520b6d4b88a938d74d8
egodd <96998522+egodd@users.noreply.github.com>
2022-09-12 14:00:04 -0400

    Update mail_handler.php

--- mail_handler.php
+++ mail_handler.php
@@ -1,20 +1,32 @@
 <?php 
-if(isset($_POST['submit'])){
-    $to = "evangoddard@brandeis.edu"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
-
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Thank you " . $first_name . ", you will be updated when the site is live!.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    // You cannot use header and echo together. It's one or the other.
-    }
+$errors = '';
+$myemail = 'evangoddard@brandeis.edu';
+if(empty($_POST['name'])  ||
+   empty($_POST['email']) ||
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+if (!preg_match(
+"/ ^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+if( empty($errors))
+{
+    $to = '$myemail';
+    $email_subject = "Contact form submission: $name";
+    $email_body = "You have received a new message. ".
+        " Here are the details:\n Name: $name \n ".
+        "Email: $email_address\n Message \n $message";
+    $headers = "From: $myemail\n";
+    $headers .= "Reply-To: $email_address";
+    mail($to,$email_subject,$email_body,$headers);
+    //redirect to the 'thank you' page
+    header('Location: contact-form-thank-you.html');
+}
 ?>
